bb40511edcab7a5a3c6b7f28b71dc00acbb3046e
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-03-06 17:09:58 +0300

    Подключение драйвера вынесено в отдельную функцию

--- section1/lesson6_step10_uniqueness.py
+++ section1/lesson6_step10_uniqueness.py
@@ -1,46 +1,61 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-try:
+
+def chrome_driver_manager() -> webdriver.Chrome:
+    """Попытка подключить менеджер для автоматизации управления драйверами,
+    в случае его отсутствия запускает драйвер установленный в системе"""
+    try:
+        from webdriver_manager.chrome import ChromeDriverManager
+    except ModuleNotFoundError:
+        return webdriver.Chrome()
+    return webdriver.Chrome(ChromeDriverManager().install())
+
+
+def main():
     link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get(link)
+    try:        
+        browser = chrome_driver_manager()
+        browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
-    input_first_name = browser.find_element_by_css_selector(
-        '.first_block .first'
-    )
-    input_first_name.send_keys("Ivan")
+        # Ваш код, который заполняет обязательные поля
+        input_first_name = browser.find_element_by_css_selector(
+            '.first_block .first'
+        )
+        input_first_name.send_keys("Ivan")
 
-    input_last_name = browser.find_element_by_css_selector(
-        '.first_block .second'
-    )
-    input_last_name.send_keys("Petrov")
+        input_last_name = browser.find_element_by_css_selector(
+            '.first_block .second'
+        )
+        input_last_name.send_keys("Petrov")
 
-    input_email = browser.find_element_by_css_selector(
-        '.first_block .third'
-    )
-    input_email.send_keys("mail@mail.com")
+        input_email = browser.find_element_by_css_selector(
+            '.first_block .third'
+        )
+        input_email.send_keys("mail@mail.com")
 
-    # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+ 
+ 
+if __name__ == '__main__':
+    main()
